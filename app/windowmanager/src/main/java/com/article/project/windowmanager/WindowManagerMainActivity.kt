fecc454211fb1d7655debc2bf011db4a6abf0705
parkjunghoon <go9018@naver.com>
2021-11-20 12:52:43 +0900

    init windowmanager codelab

---
+++ app/windowmanager/src/main/java/com/article/project/windowmanager/WindowManagerMainActivity.kt
@@ -0,0 +1,11 @@
+package com.article.project.windowmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WindowManagerMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_window_manager_main)
+    }
+}
